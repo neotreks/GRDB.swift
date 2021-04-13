a917669f81ce8d31075e9a9ec9cb01af10bd0c46
Rudolf Kopriva <ruda@neotreks.com>
2021-04-13 17:33:03 +0200

    Config changes

--- GRDB/Export.swift
+++ GRDB/Export.swift
@@ -2,7 +2,7 @@
 #if SWIFT_PACKAGE
 @_exported import CSQLite
 #elseif GRDBCIPHER
-@_exported import AccuTerraSQLCipher
+@_exported import SQLCipher
 #elseif !GRDBCUSTOMSQLITE && !GRDBCIPHER
 @_exported import SQLite3
 #endif
